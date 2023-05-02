253afe0d80be2acb5bc358a5f310ab634f40242b
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-02 22:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function removecolor()
 {
-var x=document.getElementById("colorSelect");
-x.remove(x.selectedIndex);
+var element =document.getElementById("colorSelect");
+element.remove(element.selectedIndex);
 }
